5675f0a9ee7ef1566992fd3536eea21407704007
Danylo Bida <dbidyc@gmail.com>
2024-11-27 14:55:20 +0100

    hello nazar

--- src/main/java/com/fastfashion/prototyp/Question.java
+++ src/main/java/com/fastfashion/prototyp/Question.java
@@ -1,16 +1,21 @@
 package com.fastfashion.prototyp;
 
 public class Question {
+    enum Difficulty{
+        easy, medium, hard
+    }
+    private Difficulty difficulty;
     private String question;
     private String rightClothingBarcode;
     private String explanation;
 
     public Question() {}
 
-    Question(String question, String rightClothingBarcode, String explanation){
+    Question(String question, String rightClothingBarcode, String explanation, Difficulty difficulty){
         this.question = question;
         this.rightClothingBarcode = rightClothingBarcode;
         this.explanation = explanation;
+        this.difficulty = difficulty;
     }
 
     public boolean isRight(String barcode){
